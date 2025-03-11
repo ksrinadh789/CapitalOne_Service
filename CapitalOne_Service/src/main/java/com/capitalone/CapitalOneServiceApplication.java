c64d29b3c6d41df8eeca0bd9e39976f9edcb37e3
ksrinadh789 <ksrinadh789@gmail.com>
2025-03-11 16:54:52 -0400

    Initial commit - Added CapitalOne_Service project

---
+++ CapitalOne_Service/src/main/java/com/capitalone/CapitalOneServiceApplication.java
@@ -0,0 +1,16 @@
+package com.capitalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalOneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalOneServiceApplication.class, args);
+		
+		System.out.println("\r -------------Welcome to Captial one ------------------------------");
+		
+	}
+
+}
---
+++ CapitalOne_Service/src/main/java/com/capitalone/entiry/Account.java
@@ -0,0 +1,5 @@
+package com.capitalone.entiry;
+
+public class Account {
+
+}
